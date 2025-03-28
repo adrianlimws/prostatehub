a10051c1300fcc6083ce80fe5ba8659b4294de11
Adrian Lim <adrianlimws@gmail.com>
2025-03-28 18:20:17 +1300

    added tailwind & netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vue from '@astrojs/vue';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [vue()],
+  server: {host: true, port: 3000 },
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  adapter: netlify()
+});
