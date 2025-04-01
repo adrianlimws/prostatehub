4637d21f850f54285659f0968e7b750ffdd959b3
Adrian Lim <adrianlimws@gmail.com>
2025-04-01 19:44:35 +1300

    updated menu

--- src/components/MenuButton.vue
+++ src/components/MenuButton.vue
@@ -17,14 +17,16 @@ const toggleMenu = () => {
     <div v-if="isMenuOpen" class="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded shadow-lg z-20">
       <ul>
         <li><a href="/" class="block p-3 hover:bg-gray-100">Home</a></li>
+        <li><a href="/stage-0" class="block p-3 hover:bg-gray-100">Stage 0</a></li>
         <li><a href="/stage-1" class="block p-3 hover:bg-gray-100">Stage 1</a></li>
         <li><a href="/stage-2" class="block p-3 hover:bg-gray-100">Stage 2</a></li>
         <li><a href="/stage-3" class="block p-3 hover:bg-gray-100">Stage 3</a></li>
         <li><a href="/stage-4" class="block p-3 hover:bg-gray-100">Stage 4</a></li>
-        <li><a href="/stage-0" class="block p-3 hover:bg-gray-100">Stage 0</a></li>
-        <li><a href="/support-groups" class="block p-3 hover:bg-gray-100">Support Groups</a></li>
-        <li><a href="/resources" class="block p-3 hover:bg-gray-100">Resources</a></li>
-        <li><a href="/contact" class="block p-3 hover:bg-gray-100">Contact</a></li>
+        <li><a href="/family-member" class="block p-3 hover:bg-gray-100">Family Support</a></li>
+        <li><a href="/studies" class="block p-3 hover:bg-gray-100">Studies</a></li>
+        <li><a href="/support-groups" class="block p-3 hover:bg-gray-100">Support Services</a></li>
+        <li><a href="/forum" class="block p-3 hover:bg-gray-100">PFNZ Forum</a></li>
+        <li><a href="/donation" class="block p-3 hover:bg-gray-100">PFNZ Donation</a></li>
       </ul>
     </div>
   </div>
