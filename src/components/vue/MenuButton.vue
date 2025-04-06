aaac318121cac6f07c1c6032a35cdaa5e3176ee8
Adrian Lim <adrianlimws@gmail.com>
2025-04-06 13:18:54 +1200

    updated menu

--- src/components/vue/MenuButton.vue
+++ src/components/vue/MenuButton.vue
@@ -9,8 +9,8 @@ const toggleMenu = () => {
 </script>
 
 <template>
-  <div class="relative rounded-sm p-2 text-white bg-blue hover:bg-green font-mono">
-    <button @click="toggleMenu" class="font-bold">
+  <div class="relative rounded-sm p-2 bg-blue hover:bg-green">
+    <button @click="toggleMenu" class="text-white font-bold">
       MENU
     </button>
     
@@ -19,25 +19,25 @@ const toggleMenu = () => {
 
     <!-- Sliding menu panel -->
     <div 
-      class="fixed top-0 right-0 h-full w-64 bg-blue border-l border-blue shadow-lg z-50 transform transition-transform duration-300 ease-in-out"
+      class="fixed top-0 right-0 h-full w-64 bg-fuchsia border-l border-black z-50 transform transition-transform duration-300 ease-in-out font-mono text-lg"
       :class="isMenuOpen ? 'translate-x-0' : 'translate-x-full'"
     >
-      <div class="flex justify-between items-center p-4 border-b border-blue">
-        <span class="text-white font-bold">Menu</span>
-        <button @click="toggleMenu" class="font-bold text-3xl">&times;</button>
+      <div class="flex justify-between items-center p-4 border-b-2 border-black">
+        <span class="font-bold">Menu</span>
+        <button @click="toggleMenu" class="font-bold text-3xl hover:animate-spin">&times;</button>
       </div>
       <ul>
-        <li><a href="/"               class="block p-3 text-white hover:bg-blue-500">Home</a></li>
-        <li><a href="/stage-0"        class="block p-3 text-white hover:bg-blue-500">Stage 0</a></li>
-        <li><a href="/stage-1"        class="block p-3 text-white hover:bg-blue-500">Stage 1</a></li>
-        <li><a href="/stage-2"        class="block p-3 text-white hover:bg-blue-500">Stage 2</a></li>
-        <li><a href="/stage-3"        class="block p-3 text-white hover:bg-blue-500">Stage 3</a></li>
-        <li><a href="/stage-4"        class="block p-3 text-white hover:bg-blue-500">Stage 4</a></li>
-        <li><a href="/family-member"  class="block p-3 text-white hover:bg-blue-500">Family Support</a></li>
-        <li><a href="/studies"        class="block p-3 text-white hover:bg-blue-500">Studies</a></li>
-        <li><a href="/support-groups" class="block p-3 text-white hover:bg-blue-500">Support Groups</a></li>
-        <li><a href="/forum"          class="block p-3 text-white hover:bg-blue-500">PFNZ Forum</a></li>
-        <li><a href="/donation"       class="block p-3 text-white hover:bg-blue-500">PFNZ Donation</a></li>
+        <li><a href="/"               class="block p-3 hover:bg-green hover:text-white transition">Home</a></li>
+        <li><a href="/stage-0"        class="block p-3 hover:bg-green hover:text-white transition">Stage 0</a></li>
+        <li><a href="/stage-1"        class="block p-3 hover:bg-green hover:text-white transition">Stage 1</a></li>
+        <li><a href="/stage-2"        class="block p-3 hover:bg-green hover:text-white transition">Stage 2</a></li>
+        <li><a href="/stage-3"        class="block p-3 hover:bg-green hover:text-white transition">Stage 3</a></li>
+        <li><a href="/stage-4"        class="block p-3 hover:bg-green hover:text-white transition">Stage 4</a></li>
+        <li><a href="/family-member"  class="block p-3 hover:bg-green hover:text-white transition">Family Support</a></li>
+        <li><a href="/studies"        class="block p-3 hover:bg-green hover:text-white transition">Studies</a></li>
+        <li><a href="/support-groups" class="block p-3 hover:bg-green hover:text-white transition">Support Groups</a></li> 
+        <li><a href="/forum"          class="block p-3 hover:bg-green hover:text-white transition">PFNZ Forum</a></li> 
+        <li><a href="/donation"       class="block p-3 hover:bg-green hover:text-white transition">PFNZ Donation</a></li>
       </ul>
     </div>
   </div>
