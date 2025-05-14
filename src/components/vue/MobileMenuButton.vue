dfbd8a97a8739bc5cb2efcbb0a6f65b296db12af
Adrian Lim <adrianlimws@gmail.com>
2025-05-14 20:34:46 +1200

    changed forum external link

--- src/components/vue/MobileMenuButton.vue
+++ src/components/vue/MobileMenuButton.vue
@@ -156,12 +156,12 @@ const handleResize = () => {
       <div class="bg-gray-50 py-2">
         <h3 class="px-4 text-xs font-semibold text-gray-500 uppercase tracking-wider mb-2">External Resources</h3>
         <a 
-          href="https://prostate.org.nz/online-forum" 
+          href="https://prostatecancernz.ning.com/" 
           target="_blank" 
           class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition-colors"
         >
           <div class="flex items-center">
-            <span>PFNZ Forum</span>
+            <span>PCNZ Forum</span>
             <svg class="ml-1 h-4 w-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
               <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
               <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
